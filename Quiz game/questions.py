31d044dfd2957a3c155cc26daad2c08a5f00cb40
bishwajit0 <88493070+bishwajit0@users.noreply.github.com>
2021-08-05 19:57:32 +0600

    Edited

--- Quiz game/questions.py
+++ Quiz game/questions.py
@@ -1,4 +1,4 @@
-quest = ['What is the capital of USA?', 'How many planets are there in the solar system?', 'How many continents out there?', 'Who was the fist person in the moon?']
-answers = ['Washington DC', '9', '7', "Neil"]
+questions = ['What is the capital of USA?', 'How many countries are there in UK?','How many planets are there in the solar system?', 'How many continents out there?', 'Who was the fist person in the moon?']
+answers = ['Washington DC','4', '9', '7', "Neil"]
 
-dct = dict((a, b) for a, b in zip (quest, answers))
+dct = dict((a, b) for a, b in zip (questions, answers))
--- Quiz game/quiz game.py
+++ Quiz game/quiz game.py
@@ -5,15 +5,54 @@ from questions import *
 
 q = str.lower(input("Do you wanna play? "))
 
-if q == "yes":
-    random_item = random.choice(questions)
-    a = str(input(random_item))
-    score = 0
-    if a in answers:
-        print("Your answer is correct!")
-        score += 1
-    else:
-        print("Sorry, wrong answer!")
+if q != "yes":
+   quit()
+
+print("Okay lets play!")
+score = 0
+
+random_item = random.choice(questions)
+a = str(input(random_item))
+if a in answers:
+    print("Your answer is correct!")
+    score += 1
 else:
-    quit()
-    
+    print("Sorry, wrong answer!")
+
+
+random_item = random.choice(questions)
+a = str(input(random_item))
+if a in answers:
+    print("Your answer is correct!")
+    score += 1
+else:
+    print("Sorry, wrong answer!")
+
+
+random_item = random.choice(questions)
+a = str(input(random_item))
+if a in answers:
+    print("Your answer is correct!")
+    score += 1
+else:
+    print("Sorry, wrong answer!")
+
+random_item = random.choice(questions)
+a = str(input(random_item))
+if a in answers:
+    print("Your answer is correct!")
+    score += 1
+else:
+    print("Sorry, wrong answer!")
+
+random_item = random.choice(questions)
+a = str(input(random_item))
+if a in answers:
+    print("Your answer is correct!")
+    score += 1
+else:
+    print("Sorry, wrong answer!")
+
+
+print("You got " +  str(score) + " questions correct!")
+print("You got " +  str((score/5)*100) + "%.")
